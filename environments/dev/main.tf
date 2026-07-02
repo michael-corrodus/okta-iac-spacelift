4d725033b7e73a36cf892b965c46f397b675ae00
michael-corrodus <michael.corrodus@justeattakeaway.com>
2026-07-02 11:51:22 +0100

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,123 +1,119 @@
-# 1. OIDC APP
-resource "okta_app_oauth" "test_oidc_app" {
-  label                      = "Test OIDC App"
-  type                       = "web"
-  grant_types                = ["authorization_code", "refresh_token"]
-  response_types             = ["code"]
-  redirect_uris              = ["http://localhost:3000/callback"]
-  post_logout_redirect_uris  = ["http://localhost:3000"]
+# ============================================================================
+# GROUPS (already created, but shown for reference)
+# ============================================================================
+
+resource "okta_group" "developers" {
+  name        = "Terraform Developers"
+  description = "Group created and managed by Terraform via Spacelifts"
 }
 
-resource "okta_app_group_assignment" "developers_to_oidc" {
-  app_id   = okta_app_oauth.test_oidc_app.id
+resource "okta_group" "engineers" {
+  name        = "Engineers"
+  description = "Engineering team"
+}
+
+resource "okta_group" "product" {
+  name        = "Product"
+  description = "Product team"
+}
+
+# ============================================================================
+# 1. NATIVE OIDC APP
+# ============================================================================
+
+module "native_oidc_app" {
+  source = "./modules/okta-app-native-oidc"
+
+  app_label              = "Test Native OIDC App"
+  grant_types            = ["authorization_code", "refresh_token"]
+  response_types         = ["code"]
+  redirect_uris          = ["http://localhost:3000/callback"]
+  post_logout_redirect_uris = ["http://localhost:3000"]
+}
+
+resource "okta_app_group_assignment" "developers_to_native_oidc" {
+  app_id   = module.native_oidc_app.app_id
   group_id = okta_group.developers.id
 }
 
-resource "okta_app_group_assignment" "engineers_to_oidc" {
-  app_id   = okta_app_oauth.test_oidc_app.id
+resource "okta_app_group_assignment" "engineers_to_native_oidc" {
+  app_id   = module.native_oidc_app.app_id
   group_id = okta_group.engineers.id
 }
 
-# 2. SAML APP
-resource "web_saml" "test_saml_app" {
-  label                = "Test SAML App"
-  single_sign_on_url             = "http://localhost:3000/saml/consume"
-  recipient                      = "http://localhost:3000/saml/consume"
-  destination                    = "http://localhost:3000/saml/consume"
-  audience                       = "http://localhost:3000"
-  subject_name_id_format         = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
-  subject_name_id_template       = "${user.email}"
-  response_signed                = true
-  signature_algorithm            = "RSA_SHA256"
-  digest_algorithm               = "SHA256"
-  
-  attribute_statements {
-    type      = "EXPRESSION"
-    name      = "email"
-    values    = ["user.email"]
-  }
-  
-  attribute_statements {
-    type      = "EXPRESSION"
-    name      = "firstName"
-    values    = ["user.firstName"]
-  }
-  
-  attribute_statements {
-    type      = "EXPRESSION"
-    name      = "lastName"
-    values    = ["user.lastName"]
-  }
+# ============================================================================
+# 2. SPA OIDC APP
+# ============================================================================
+
+module "spa_oidc_app" {
+  source = "./modules/okta-app-spa-oidc"
+
+  app_label              = "Test SPA OIDC App"
+  grant_types            = ["authorization_code", "refresh_token"]
+  response_types         = ["code"]
+  redirect_uris          = ["http://localhost:3001/callback"]
+  post_logout_redirect_uris = ["http://localhost:3001"]
 }
 
-resource "okta_app_group_assignment" "product_to_saml" {
-  app_id   = okta_app_saml.test_saml_app.id
+resource "okta_app_group_assignment" "product_to_spa_oidc" {
+  app_id   = module.spa_oidc_app.app_id
   group_id = okta_group.product.id
 }
 
-resource "okta_app_group_assignment" "developers_to_saml" {
-  app_id   = okta_app_saml.test_saml_app.id
+resource "okta_app_group_assignment" "developers_to_spa_oidc" {
+  app_id   = module.spa_oidc_app.app_id
   group_id = okta_group.developers.id
 }
 
-# 3. PRECONFIG APP
-resource "web_saml" "test_preconfig_app" {
-  label                = "Test Preconfig App (Slack)"
-  single_sign_on_url             = "https://yourslack.slack.com/sso/saml"
-  recipient                      = "https://yourslack.slack.com/sso/saml"
-  destination                    = "https://yourslack.slack.com/sso/saml"
-  audience                       = "https://yourslack.slack.com"
-  subject_name_id_format         = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
-  subject_name_id_template       = "${user.email}"
-  response_signed                = true
-  signature_algorithm            = "RSA_SHA256"
-  
-  attribute_statements {
-    type      = "EXPRESSION"
-    name      = "email"
-    values    = ["user.email"]
-  }
-  
-  attribute_statements {
-    type      = "EXPRESSION"
-    name      = "firstName"
-    values    = ["user.firstName"]
-  }
-  
-  attribute_statements {
-    type      = "EXPRESSION"
-    name      = "lastName"
-    values    = ["user.lastName"]
-  }
+# ============================================================================
+# 3. WEB OIDC APP
+# ============================================================================
+
+module "web_oidc_app" {
+  source = "./modules/okta-app-web-oidc"
+
+  app_label              = "Test Web OIDC App"
+  grant_types            = ["authorization_code", "refresh_token"]
+  response_types         = ["code"]
+  redirect_uris          = ["http://localhost:3002/callback"]
+  post_logout_redirect_uris = ["http://localhost:3002"]
 }
 
-resource "okta_app_group_assignment" "engineers_to_preconfig" {
-  app_id   = okta_app_saml.test_preconfig_app.id
+resource "okta_app_group_assignment" "engineers_to_web_oidc" {
+  app_id   = module.web_oidc_app.app_id
   group_id = okta_group.engineers.id
 }
 
-resource "okta_app_group_assignment" "product_to_preconfig" {
-  app_id   = okta_app_saml.test_preconfig_app.id
+resource "okta_app_group_assignment" "product_to_web_oidc" {
+  app_id   = module.web_oidc_app.app_id
   group_id = okta_group.product.id
 }
 
-output "oidc_app_client_id" {
-  value       = okta_app_oauth.test_oidc_app.client_id
-  description = "OIDC App Client ID"
+# ============================================================================
+# 4. WEB SAML APP
+# ============================================================================
+
+module "web_saml_app" {
+  source = "./modules/okta-app-web-saml"
+
+  app_label            = "Test Web SAML App"
+  sso_url              = "http://localhost:3003/saml/consume"
+  recipient            = "http://localhost:3003/saml/consume"
+  destination          = "http://localhost:3003/saml/consume"
+  audience             = "http://localhost:3003"
 }
 
-output "oidc_app_client_secret" {
-  value       = okta_app_oauth.test_oidc_app.client_secret
-  sensitive   = true
-  description = "OIDC App Client Secret"
+resource "okta_app_group_assignment" "developers_to_web_saml" {
+  app_id   = module.web_saml_app.app_id
+  group_id = okta_group.developers.id
 }
 
-output "saml_app_metadata_url" {
-  value       = okta_app_saml.test_saml_app.metadata_url
-  description = "SAML App Metadata URL"
+resource "okta_app_group_assignment" "engineers_to_web_saml" {
+  app_id   = module.web_saml_app.app_id
+  group_id = okta_group.engineers.id
 }
 
-output "preconfig_app_metadata_url" {
-  value       = okta_app_saml.test_preconfig_app.metadata_url
-  description = "Preconfig App Metadata URL"
+resource "okta_app_group_assignment" "product_to_web_saml" {
+  app_id   = module.web_saml_app.app_id
+  group_id = okta_group.product.id
 }
