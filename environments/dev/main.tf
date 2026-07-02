e1ff6c240db12cc0bfb8538475097a7684d9439d
michael-corrodus <michael.corrodus@justeattakeaway.com>
2026-07-02 11:03:58 +0100

    Update main.tf
    new salesforce app

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,11 +1,14 @@
-module "dev_group" {
-  source            = "../../modules/okta-groups"
-  group_name        = "dev-team"
-  group_description = "Development team group"
+module "Salesforce_app" {
+  source        = "./modules/okta-app-web-saml"
+  app_label     = "Salesforce Global"
+  sso_url     = "https://salesforce.global.com"
+  recipient   = "https://salesforce.global.com"
+  destination = "https://salesforce.global.com"
+  audience    = "https://salesforce.global.com"
 }
 
-module "dev_web_app" {
-  source        = "../../modules/okta-app-web-oidc"
-  app_label     = "dev-web-app"
-  redirect_uris = ["https://dev.example.com/callback"]
+module "dev_group" {
+  source            = "../../modules/okta-groups"
+  group_name        = "Salesforce Global Users"
+  group_description = "Salesforce Users Group"
 }
