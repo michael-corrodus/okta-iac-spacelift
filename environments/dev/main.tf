47bfdf0a177a492aa0baf0c605615e97b1cb4a52
michael-corrodus <michael.corrodus@justeattakeaway.com>
2026-07-02 11:34:36 +0100

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,22 +1,7 @@
-# Direct resource - no modules needed for testing
-resource "okta_group" "developers" {
-  name        = "Terraform Developers"
-  description = "Group created and managed by Terraform via Spacelifts"
-}
-
-resource "okta_group" "engineers" {
-  name        = "Engineers"
-  description = "Engineering team"
-}
-
-resource "okta_group" "product" {
-  name        = "Product"
-  description = "Product team"
-}
-
-# Create an OIDC application
-resource "okta_app_oauth" "my_app" {
-  label                      = "My Test App"
+# 1. OIDC APP (Web Application)
+# ============================================================================
+resource "okta_app_oauth" "test_oidc_app" {
+  label                      = "Michael Marketing OIDC App"
   type                       = "web"
   grant_types                = ["authorization_code", "refresh_token"]
   response_types             = ["code"]
@@ -24,14 +9,110 @@ resource "okta_app_oauth" "my_app" {
   post_logout_redirect_uris  = ["http://localhost:3000"]
 }
 
-# Assign the Developers group to the app
-resource "okta_app_group_assignment" "developers_to_app" {
-  app_id   = okta_app_oauth.my_app.id
+# Assign groups to OIDC app
+resource "okta_app_group_assignment" "developers_to_oidc" {
+  app_id   = okta_app_oauth.test_oidc_app.id
   group_id = okta_group.developers.id
 }
 
-# Assign the Engineers group to the app
-resource "okta_app_group_assignment" "engineers_to_app" {
-  app_id   = okta_app_oauth.my_app.id
+resource "okta_app_group_assignment" "engineers_to_oidc" {
+  app_id   = okta_app_oauth.test_oidc_app.id
   group_id = okta_group.engineers.id
+}
+
+---
+
+# 2. SAML APP (Web/Enterprise Application)
+# ============================================================================
+resource "okta_app_saml" "test_saml_app" {
+  label                = "Michael Sales App"
+  
+  # SAML Config
+  single_sign_on_url             = "http://localhost:3000/saml/consume"
+  recipient                      = "http://localhost:3000/saml/consume"
+  destination                    = "http://localhost:3000/saml/consume"
+  audience                       = "http://localhost:3000"
+  subject_name_id_format         = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+  subject_name_id_template       = "${user.email}"
+  response_signed                = true
+  signature_algorithm            = "RSA_SHA256"
+  digest_algorithm               = "SHA256"
+  
+  # Attribute mappings
+  attribute_statements {
+    type      = "EXPRESSION"
+    name      = "email"
+    values    = ["user.email"]
+  }
+  
+  attribute_statements {
+    type      = "EXPRESSION"
+    name      = "firstName"
+    values    = ["user.firstName"]
+  }
+  
+  attribute_statements {
+    type      = "EXPRESSION"
+    name      = "lastName"
+    values    = ["user.lastName"]
+  }
+}
+
+# Assign groups to SAML app
+resource "okta_app_group_assignment" "product_to_saml" {
+  app_id   = okta_app_saml.test_saml_app.id
+  group_id = okta_group.product.id
+}
+
+resource "okta_app_group_assignment" "developers_to_saml" {
+  app_id   = okta_app_saml.test_saml_app.id
+  group_id = okta_group.developers.id
+}
+
+---
+
+# 3. PRECONFIG APP (SaaS Application - Slack Example)
+# ============================================================================
+resource "okta_app_saml" "test_preconfig_app" {
+  label                = "Michael App (Slack)"
+  
+  # Single sign-on URL (Slack example)
+  single_sign_on_url             = "https://yourslack.slack.com/sso/saml"
+  recipient                      = "https://yourslack.slack.com/sso/saml"
+  destination                    = "https://yourslack.slack.com/sso/saml"
+  audience                       = "https://yourslack.slack.com"
+  subject_name_id_format         = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+  subject_name_id_template       = "${user.email}"
+  response_signed                = true
+  signature_algorithm            = "RSA_SHA256"
+  
+  # Slack attribute mappings
+  attribute_statements {
+    type      = "EXPRESSION"
+    name      = "email"
+    values    = ["user.email"]
+  }
+  
+  attribute_statements {
+    type      = "EXPRESSION"
+    name      = "firstName"
+    values    = ["user.firstName"]
+  }
+  
+  attribute_statements {
+    type      = "EXPRESSION"
+    name      = "lastName"
+    values    = ["user.lastName"]
+  }
+}
+
+# Assign groups to preconfig app
+resource "okta_app_group_assignment" "engineers_to_preconfig" {
+  app_id   = okta_app_saml.test_preconfig_app.id
+  group_id = okta_group.engineers.id
+}
+
+resource "okta_app_group_assignment" "product_to_preconfig" {
+  app_id   = okta_app_saml.test_preconfig_app.id
+  group_id = okta_group.product.id
 }
