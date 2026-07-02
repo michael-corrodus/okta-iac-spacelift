354e9b117fe585f2c989944670b9306bf1647738
michael-corrodus <michael.corrodus@justeattakeaway.com>
2026-07-02 13:51:32 +0100

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,29 @@
+# Keep this old code
+resource "okta_app_oauth" "my_app" {
+  label                      = "My Test App"
+  type                       = "web"
+  grant_types                = ["authorization_code", "refresh_token"]
+  response_types             = ["code"]
+  redirect_uris              = ["http://localhost:3000/callback"]
+  post_logout_redirect_uris  = ["http://localhost:3000"]
+}
+
+resource "okta_app_group_assignment" "developers_to_app" {
+  app_id   = okta_app_oauth.my_app.id
+  group_id = okta_group.developers.id
+}
+
+resource "okta_app_group_assignment" "engineers_to_app" {
+  app_id   = okta_app_oauth.my_app.id
+  group_id = okta_group.engineers.id
+}
+
+# PLUS your new module code
+module "native_oidc_app" {
+  source = "../../modules/okta-app-native-oidc"
+  ...
+}
+
 module "web_saml_app" {
 source = "../../modules/okta-app-web-saml"
 
