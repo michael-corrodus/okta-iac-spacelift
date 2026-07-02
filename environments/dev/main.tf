fc616edef5be01b5b3742dd0dafed03f2ab7fb70
michael-corrodus <michael.corrodus@justeattakeaway.com>
2026-07-02 13:59:07 +0100

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -34,12 +34,13 @@ source = "../../modules/okta-app-web-saml"
   audience             = "http://localhost:3003"
 }
 
+resource "okta_group" "engineers" {
+  name        = "Terraform Engineers"
+  description = "Group created and managed by Terraform via Spacelifts"
+}
+
 resource "okta_group" "developers" {
   name        = "Terraform Developers"
   description = "Group created and managed by Terraform via Spacelifts"
 }
 
-resource "okta_app_group_assignment" "developers_to_web_saml" {
-  app_id   = module.web_saml_app.app_id
-  group_id = okta_group.developers.id
-}
