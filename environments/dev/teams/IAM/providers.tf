3981c5ea49f5715b1a5740035ba28868e2d34936
michael-corrodus <michael.corrodus@justeattakeaway.com>
2026-07-10 11:32:03 +0100

    fix repo

---
+++ environments/dev/teams/IAM/providers.tf
@@ -0,0 +1,22 @@
+variable "okta_org_name" {}
+variable "okta_base_url" {
+  description = "okta.com | okta-emea.com | oktapreview.com"
+  default     = "okta.com"
+}
+variable "okta_api_token" {
+  sensitive = true
+}
+
+variable "okta_max_api_capacity" {
+  description = "Throttle Okta provider requests as a percentage of org capacity (10-100)."
+  type        = number
+  default     = 50
+}
+
+provider "okta" {
+  max_api_capacity = var.okta_max_api_capacity
+  org_name         = var.okta_org_name
+  base_url         = var.okta_base_url
+  api_token        = var.okta_api_token
+
+}
---
+++ environments/dev/teams/OPS/versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    okta = {
+      source  = "okta/okta"
+      version = "= 6.1.0"
+    }
+    // ...existing provider entries...
+  }
+}
