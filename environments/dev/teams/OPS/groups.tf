43e9d3004367d19ab5ac106e5e76e5f263fe3a19
michael-corrodus <michael.corrodus@justeattakeaway.com>
2026-07-11 05:23:31 +0100

    adding slack app

--- environments/dev/teams/OPS/groups.tf
+++ environments/dev/teams/OPS/groups.tf
@@ -2,3 +2,8 @@ resource "okta_group" "freshservice_users" {
   name        = "Freshservice Users"
   description = "Freshservice application users"
 }
+
+resource "okta_group" "slack_users" {
+  name        = "Slack Users"
+  description = "Users assigned to Slack App"
+}
