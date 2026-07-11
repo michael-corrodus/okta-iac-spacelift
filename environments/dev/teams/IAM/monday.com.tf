0f194488720759d008652a67a708b2dc7bdd6b23
michael-corrodus <michael.corrodus@justeattakeaway.com>
2026-07-11 05:00:53 +0100

    correcting module

--- environments/dev/teams/IAM/monday.com.tf
+++ environments/dev/teams/IAM/monday.com.tf
@@ -7,6 +7,6 @@ module "mondaycom" {
 }
 
 resource "okta_app_group_assignment" "monday_users" {
-  app_id   = module.monday_users_id
+  app_id   = module.mondaycom_app_id
   group_id = okta_group.monday_users.id
 }
